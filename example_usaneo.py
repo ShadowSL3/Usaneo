4e65ba70e0895740fcfb91d86a0504aefd74ca54
ShadowSL3 <mizasp71@gmail.com>
2024-08-08 11:51:34 +0000

    Removed Python 3.8 3.9

--- example_usaneo.py
+++ example_usaneo.py
@@ -1,19 +1,11 @@
 from usaneo.core import usaneo_init, usaneo_quit, usaneo_update, usaneo_handle_events
 from usaneo.sprites import UsaneoSprite, UsaneoFont
+from usaneo.fast_event import FastEvent_Core
 def main():
-    screen = dont_init(800, 600)
-    sprite = UsaneoSprite('path/to/sprite.png')
-    font = UsaneoFont('path/to/font.ttf', 24)
-
+    screen = usaneo_init(800, 600)
     running = True
     while running:
         running = usaneo_handle_events()
-        
-        screen.fill((255, 255, 255))
-        sprite.draw(screen)
-        text_surface = font.render("Hello, Usaneo!", (0, 0, 0))
-        screen.blit(text_surface, (50, 50))
-        
         usaneo_update()
     
     usaneo_quit()
