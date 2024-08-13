0130dc1aa50448a34f136be0937b62656e26dab2
ShadowSL3 <mizasp71@gmail.com>
2024-08-13 20:18:57 +0000

    Updated New Vector3 Addes __init__ For start Class Vec2 and 3

---
+++ usaneo/utils/maths.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Vec2:
+     def __init__(self, vec2):
+        self.vec2 = pygame.math.Vector2
+
+
+class Vec3:
+    def __init__(self, vec3):
+        self.vec3 = pygame.math.Vector3
